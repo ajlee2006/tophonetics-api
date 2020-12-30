94be65369f86088108708ddb9316a70e62d7c820
AJ Lee <ajlee2006@gmail.com>
2020-12-30 14:31:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Type in "/api?text=" and then some text.<br>You can also change the dialect to American by adding "&dialect=am" after the text.' + '<img src="https://s01.flagcounter.com/count2/Ya4c/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" border="0" height="0" width="0">'
+    return 'Type in "/api?text=" and then some text.<br>You can also change the dialect to American by adding "&dialect=am" after the text.'
 
 @app.route('/api')
 def toipa():
     try:
         text = request.form['text']
     except:
-        text = request.args.get('text', '')
+        try:
+            text = request.args.get('text', '')
+        except:
+            return 'Type in "/api?text=" and then some text.<br>You can also change the dialect to American by adding "&dialect=am" after the text.'
     try:
         dialect = request.form['dialect']
     except:
